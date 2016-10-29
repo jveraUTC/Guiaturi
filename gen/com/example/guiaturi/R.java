06e8b66428f44a8eb7fd38289584249f4449dddc
echangoUTC <evelynforever00@outlook.com>
2016-10-28 20:39:58 -0500

    GrupoAechangoUTC3
    colocacion de las cabeceras

--- gen/com/example/guiaturi/R.java
+++ gen/com/example/guiaturi/R.java
@@ -29,10 +29,11 @@ public final class R {
         public static final int cotopaxi=0x7f020004;
         public static final int fondo=0x7f020005;
         public static final int fondo1=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int linea=0x7f020008;
-        public static final int lugares=0x7f020009;
-        public static final int tren=0x7f02000a;
+        public static final int fondo2=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int linea=0x7f020009;
+        public static final int lugares=0x7f02000a;
+        public static final int tren=0x7f02000b;
     }
     public static final class id {
         public static final int AbsoluteLayout1=0x7f080000;
