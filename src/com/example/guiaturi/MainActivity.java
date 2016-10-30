7aa21279fbfdbe64cba1c28b5abbbaa375dfdcbc
Bfabarautc <belenfabarav93@gmail.com>
2016-10-29 20:12:43 -0500

    expand list

--- src/com/example/guiaturi/MainActivity.java
+++ src/com/example/guiaturi/MainActivity.java
@@ -4,33 +4,84 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private List<String> listGroup;
+	private HashMap<String, List<String>> listData;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		buildList();
+		
+		ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+		expandableListView.setAdapter(new ExpandableAdapter(MainActivity.this, listGroup, listData));
+		
+		expandableListView.setOnChildClickListener(new OnChildClickListener(){
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+				Toast.makeText(MainActivity.this, "Group: "+groupPosition+"| Item: "+childPosition, Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		});
+		
+		expandableListView.setOnGroupExpandListener(new OnGroupExpandListener(){
+			@Override
+			public void onGroupExpand(int groupPosition) {
+				Toast.makeText(MainActivity.this, "Group (Expand): "+groupPosition, Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		expandableListView.setOnGroupCollapseListener(new OnGroupCollapseListener(){
+			@Override
+			public void onGroupCollapse(int groupPosition) {
+				Toast.makeText(MainActivity.this, "Group (Collapse): "+groupPosition, Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		expandableListView.setGroupIndicator(getResources().getDrawable(R.drawable.icon_group));
+	}
+	
+	public void buildList(){
+		listGroup = new ArrayList<String>();
+		listData = new HashMap<String, List<String>>();
+		
+		// GROUP
+			listGroup.add("A donde quieres ir ?");
+			
+		// CHILDREN
+			List<String> auxList = new ArrayList<String>();
+			auxList.add("Latacunga");
+			auxList.add("Pujili");
+			auxList.add("Salcedo");
+			auxList.add("Item 4");
+			listData.put(listGroup.get(0), auxList);
+			
+			
+	}
 }
