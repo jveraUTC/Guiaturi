e123c3ce5b4e82d84b9f764106647a838b13d80f
GeoAlex1994 <alex.rivera2@utc.edu.ec>
2016-10-29 21:29:46 -0500

    Cambio1

--- gen/com/example/guiaturi/R.java
+++ gen/com/example/guiaturi/R.java
@@ -46,43 +46,38 @@ public final class R {
     }
     public static final class id {
         public static final int AbsoluteLayout1=0x7f080000;
-        public static final int AbsoluteLayout2=0x7f08000a;
-        public static final int GridLayout1=0x7f080009;
+        public static final int AbsoluteLayout2=0x7f08000d;
         public static final int TextView01=0x7f080007;
         public static final int TextView02=0x7f080008;
         public static final int TextView03=0x7f080005;
-        public static final int action_settings=0x7f08001d;
-        public static final int button1=0x7f08000d;
-        public static final int button2=0x7f08000e;
-        public static final int button3=0x7f08000f;
+        public static final int action_settings=0x7f08001a;
+        public static final int button1=0x7f080009;
+        public static final int button2=0x7f08000a;
+        public static final int button3=0x7f08000b;
         public static final int contacto=0x7f080004;
-        public static final int imageButton1=0x7f080010;
-        public static final int imageButton2=0x7f080011;
-        public static final int imageButton3=0x7f080013;
-        public static final int imageView1=0x7f08000b;
-        public static final int imageView2=0x7f08000c;
-        public static final int imageView3=0x7f080016;
-        public static final int imageView4=0x7f08001a;
+        public static final int imageButton1=0x7f08000f;
+        public static final int imageButton2=0x7f080010;
+        public static final int imageButton3=0x7f080012;
+        public static final int imageView1=0x7f08000c;
+        public static final int imageView2=0x7f08000e;
+        public static final int imageView3=0x7f080015;
+        public static final int imageView4=0x7f080019;
         public static final int inicio=0x7f080002;
         public static final int linearLayout1=0x7f080001;
         public static final int sitiost=0x7f080003;
         public static final int textView1=0x7f080006;
-        public static final int textView2=0x7f080012;
-        public static final int textView3=0x7f080015;
-        public static final int textView4=0x7f080014;
-        public static final int textView5=0x7f080017;
-        public static final int textView6=0x7f080018;
-        public static final int textView7=0x7f080019;
-        public static final int tvGroup=0x7f08001b;
-        public static final int tvItem=0x7f08001c;
+        public static final int textView2=0x7f080011;
+        public static final int textView3=0x7f080014;
+        public static final int textView4=0x7f080013;
+        public static final int textView5=0x7f080016;
+        public static final int textView6=0x7f080017;
+        public static final int textView7=0x7f080018;
     }
     public static final class layout {
         public static final int activity_contacto=0x7f030000;
-        public static final int activity_inicio=0x7f030001;
+        public static final int activity_main=0x7f030001;
         public static final int activity_sitio=0x7f030002;
         public static final int activity_turismo=0x7f030003;
-        public static final int header_expandable_list_view=0x7f030004;
-        public static final int item_expandable_list_view=0x7f030005;
     }
     public static final class menu {
         public static final int contacto=0x7f070000;
